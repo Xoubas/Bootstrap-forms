db35e9f1310ea396d82c86a8faac6b1d063d1278
Xoubas <a21javierbq@iessanclemente.net>
2023-02-14 11:27:59 +0100

    Retocar

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-sisi(){
-    
+function sisi() {
+    let alert = document.getElementById("alert");
+    let alert2 = document.getElementById("alert2");
+
+    if(fname.value=="") {
+        alert.style.visibility = "visible";
+        alert.classList = "alert alert-danger alert-dismissible"
+    }
+    else {
+        alert.style.visibility = "hidden";
+    }
+
+    if(lname.value=="") {
+        alert2.style.visibility = "visible";
+        alert2.classList = "alert alert-danger alert-dismissible"
+    }
+    else {
+        alert2.style.visibility = "hidden";
+    }
 }
